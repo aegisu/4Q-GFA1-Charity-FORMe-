7abf346b38f668dba35504f460a7b5a2461bede1
Knight A <126755960+aegisu@users.noreply.github.com>
2023-04-22 22:34:04 +0800

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,9 @@
+      function resetForm() {
+    document.getElementById("firstName").value = "";
+    document.getElementById("lastName").value = "";
+    document.getElementById("male").checked = false;
+    document.getElementById("female").checked = false;
+    document.getElementById("other").checked = false;
+    document.getElementById("email").value = "";
+    document.getElementById("contactNo").value = "";
+}
